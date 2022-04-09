b3f1e5f7253c80031f92ad803c9801d56990a74c
syuan7492 <kurosaki7492@gmail.com>
2022-04-09 15:48:05 +0800

    Add project files.

---
+++ ConsoleApp1/ChooseSomeDouble.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseDouble
+{
+    class ChooseSomeDouble
+    {
+
+        public static double[] PickSomeNumber(int number)
+        {
+            Random m_random = new Random();
+            double[] m_randomDouble = new double[number];
+            for (int i = 0; i < number; i++)
+            {
+                double m_value = m_random.NextDouble();
+                m_randomDouble[i] = m_value;
+            }
+            return m_randomDouble;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChooseDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入要選取的Double數量: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int number))
+            {
+                foreach (double m_randomDouble in ChooseSomeDouble.PickSomeNumber(number))
+                {
+                    Console.WriteLine(m_randomDouble);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("請輸入有效的數字");
+            }
+        }
+    }
+}
